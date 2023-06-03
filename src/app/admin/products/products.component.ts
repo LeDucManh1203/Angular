f6e0dca012d9aec0c5164faabe42d0d35d0235a0
LeManh <manhzxc3@gmail.com>
2023-06-03 08:19:30 +0700

    add: add db and data dump

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { IProduct } from 'src/common/product';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-
+  products: IProduct[] = [];
+  constructor(private productService: ProductService) {
+    this.productService.getAll().subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (error) => console.log(error.message)
+    );
+  }
 }
